5e0b5125d1bd2e032f487d247f97c3da00521dc9
emiliocramer <emilio.cramer101@gmail.com>
2024-04-04 14:57:19 -0400

    reduce song length

--- api/huggingface.mjs
+++ api/huggingface.mjs
@@ -15,7 +15,7 @@ export async function run(promptUsed) {
 		"Default", // string in 'Decoder' Radio component
 		promptUsed, // string in 'Input Text' Textbox component
 		null,
-		60, // number (numeric value between 1 and 120) in 'Duration' Slider component
+		10, // number (numeric value between 1 and 120) in 'Duration' Slider component
 		5, // number in 'Top-k' Number component
 		5, // number in 'Top-p' Number component
 		5, // number in 'Temperature' Number component
