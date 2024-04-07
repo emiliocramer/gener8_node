826dd9c88daf2e1a84734796a25c9feaaced420b
emiliocramer <emilio.cramer101@gmail.com>
2024-04-07 11:18:56 -0400

    change route

--- api/routes/audio.js
+++ api/routes/audio.js
@@ -12,8 +12,7 @@ router.get('/fetch/:jobId', async (req, res) => {
             return res.status(404).send('Audio with job ID not found');
         }
 
-        res.redirect(audio.audioUrl);
-
+        res.status(200).json(audio);
     } catch (error) {
         console.error('Fetch Error:', error);
         res.status(500).send(error.message);
